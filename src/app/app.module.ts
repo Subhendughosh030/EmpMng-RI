d637c517282195f4f78248e6a253e42d5f325457
SubhenduOnDell(E5470) <subhendughosh030@gmail.com>
2024-12-15 23:48:05 +0530

    delete 'home' and 'title' comp.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { TitleBarComponent } from './components/title-bar/title-bar.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    TitleBarComponent
   ],
   imports: [
     BrowserModule,
