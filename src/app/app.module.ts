af2b673e01d0cb8fbdf5dfaa3631e3d1352ed36d
SubhenduOnDell(E5470) <subhendughosh030@gmail.com>
2024-12-15 22:50:06 +0530

    add 'add-routing' module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
+import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 
 
@@ -13,7 +14,8 @@ import { HomeComponent } from './components/home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
