d25a0aa577aa2d34e953a78282e3ab106a17c503
SubhenduOnDell(E5470) <subhendughosh030@gmail.com>
2024-12-16 00:02:56 +0530

    generate 'emp-list' comp.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () =>
+            import('./components/emp-list/emp-list.component').then(m => m.EmpListComponent)
+    }
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/emp-list/emp-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emp-list',
+  standalone: true,
+  templateUrl: './emp-list.component.html',
+  styleUrls: ['./emp-list.component.css']
+})
+export class EmpListComponent {
+
+}
